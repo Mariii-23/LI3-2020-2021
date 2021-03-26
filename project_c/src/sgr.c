6c2eb1d355034659fb4e31a4c97d785f60ffaf8a
Matide Bravo <matildeopbravo@gmail.com>
2021-03-26 19:13:47 +0000

    Created a sort of draft for load_sgr and stats module

--- project_c/src/sgr.c
+++ project_c/src/sgr.c
@@ -1,1 +1,36 @@
-#include "sgr.h"
+#include <sgr.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "model/businesses.h"
+#include "model/reviews.h"
+#include "model/users.h"
+#include "stats.h"
+
+struct sgr {
+    UserCollection catalogo_users;
+    BusinessCollection catalogo_businesses;
+    ReviewCollection catalogo_reviews;
+    Stats estatisticas;
+};
+struct TABLE {};
+
+SGR load_sgr(char* users, char* businesses, char* reviews) {
+    FILE* fp_users = fopen(users, "r");
+    FILE* fp_businesses = fopen(businesses, "r");
+    FILE* fp_reviews = fopen(reviews, "r");
+
+    SGR sgr = malloc(sizeof(struct sgr));
+    //*sgr = (struct sgr){
+    //    .catalogo_users = ler_users(fp_users),
+    //    .catalogo_businesses = ler_businesses(fp_businesses),
+    //    .catalogo_reviews = ler_reviews(catalogo_reviews)};
+    // sgr->estatisticas = calcula_estatisticas(sgr);
+
+    fclose(fp_users);
+    fclose(fp_businesses);
+    fclose(fp_reviews);
+    return sgr;
+}
+
+int main(int argc, char* argv[]) {}
