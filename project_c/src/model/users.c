80376140b7920c968607317c048c7aa823c05c0e
Matide Bravo <matildeopbravo@gmail.com>
2021-03-26 17:11:56 +0000

    Added getters for user module

--- project_c/src/model/users.c
+++ project_c/src/model/users.c
@@ -1,13 +1,31 @@
 #include "users.h"
+
 #include <glib.h>
+#include <string.h>
 
 struct user {
-  char *user_id;
-  char *name;
-  GPtrArray *friends;
+    char* user_id;
+    char* name;
+    GPtrArray* friends;  // char* []
 };
 
 struct user_collection {
-  GArray *users;     // struct user[]
-  GHashTable *by_id; // user_id -> *user
+    GArray* users;      // struct user[]
+    GHashTable* by_id;  // <user_id*,user*>
 };
+
+char const* get_user_id(User user) {
+    return user->user_id;
+}
+char const* get_name(User user) {
+    return user->name;
+}
+GPtrArray const* get_friends(User user) {
+    return user->friends;
+}
+GArray const* get_users(UserCollection collection) {
+    return collection->users;
+}
+User const* get_user_by_id(UserCollection user_collection, int* user_id) {
+    return g_hash_table_lookup(user_collection->by_id, user_id);
+}
--- project_c/src/model/users.h
+++ project_c/src/model/users.h
@@ -1,7 +1,15 @@
 #ifndef UTILIZADORES_H
 #define UTILIZADORES_H
 
-typedef struct user *User;
-typedef struct user_collection *UserCollection;
+#include <glib.h>
+
+typedef struct user* User;
+typedef struct user_collection* UserCollection;
+
+char const* get_user_id(User user);
+char const* get_name(User user);
+GPtrArray const* get_friends(User user);
+GArray const* get_users(UserCollection user_collection);
+User const* get_user_by_id(UserCollection user_collection, int* user_id);
 
 #endif
