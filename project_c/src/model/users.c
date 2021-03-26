f6b6d378360745863df73584af264325f0163028
Matide Bravo <matildeopbravo@gmail.com>
2021-03-26 18:37:49 +0000

    Added some common functions for module users, such as create, destroy and add

--- project_c/src/model/users.c
+++ project_c/src/model/users.c
@@ -6,26 +6,68 @@
 struct user {
     char* user_id;
     char* name;
-    GPtrArray* friends;  // char* []
+    GPtrArray* friends;  // char* user_id []
 };
 
 struct user_collection {
-    GArray* users;      // struct user[]
-    GHashTable* by_id;  // <user_id*,user*>
+    GArray* users;      // User[]
+    GHashTable* by_id;  // <char* user_id,User user*>
 };
 
+User create_user(char* user_id, char* name, GPtrArray* friends) {
+    User new_user = malloc(sizeof(struct user));
+    *new_user = (struct user){
+        .user_id = g_strdup(user_id),
+        .name = g_strdup(name),
+        .friends = friends};
+    return new_user;
+}
+
+UserCollection create_collection(GArray* users, GHashTable* by_id) {
+    UserCollection new_collection = malloc(sizeof(struct user_collection));
+    *new_collection = (struct user_collection){.users = users, .by_id = by_id};
+    return new_collection;
+}
+
+void free_user(User user) {
+    free(user->user_id);
+    free(user->name);
+    g_ptr_array_set_free_func(user->friends, free);
+    g_ptr_array_free(user->friends, TRUE);
+}
+
+void free_user_collection(UserCollection user_collection) {
+    g_array_free(user_collection->users, TRUE);
+    g_hash_table_destroy(
+        user_collection->by_id);  //  calls GDestroyNotify function
+}
+
+void add_user(UserCollection user_collection, User user) {
+    g_array_append_vals(user_collection->users, user, 1);
+    g_hash_table_insert(user_collection->by_id, user->user_id, user);
+}
+
+void add_friend(User user, char* user_id) {
+    g_ptr_array_add(user->friends, g_strdup(user_id));
+}
 char const* get_user_id(User user) {
     return user->user_id;
 }
-char const* get_name(User user) {
+char const* get_user_name(User user) {
     return user->name;
 }
-GPtrArray const* get_friends(User user) {
+GPtrArray* get_friends(User user) {
     return user->friends;
 }
-GArray const* get_users(UserCollection collection) {
+GArray* get_users(UserCollection collection) {
     return collection->users;
 }
-User const* get_user_by_id(UserCollection user_collection, int* user_id) {
+User* get_user_by_id(UserCollection user_collection, int* user_id) {
     return g_hash_table_lookup(user_collection->by_id, user_id);
+}
+void set_user_id(User user, char* user_id) {
+    user->user_id = g_strdup(user_id);
+}
+void set_name(User user, char* name) {
+    user->name = g_strdup(name);
 }
--- project_c/src/model/users.h
+++ project_c/src/model/users.h
@@ -6,10 +6,18 @@
 typedef struct user* User;
 typedef struct user_collection* UserCollection;
 
+User create_user(char* user_id, char* name, GPtrArray* friends);
+UserCollection create_collection(GArray* users, GHashTable* by_id);
+void free_user(User user);
+void free_user_collection(UserCollection user_collection);
+void add_user(UserCollection user_collection, User user);
+void add_friend(User user, char* user_id);
 char const* get_user_id(User user);
-char const* get_name(User user);
-GPtrArray const* get_friends(User user);
-GArray const* get_users(UserCollection user_collection);
-User const* get_user_by_id(UserCollection user_collection, int* user_id);
+char const* get_user_name(User user);
+GPtrArray* get_friends(User user);
+GArray* get_users(UserCollection user_collection);
+User* get_user_by_id(UserCollection user_collection, int* user_id);
+void set_user_id(User user, char* user_id);
+void set_name(User user, char* name);
 
 #endif
