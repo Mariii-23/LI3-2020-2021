f3455b9625cff102b44ef911226a0b983d731b33
Mariii-23 <marianadinis8@hotmail.com>
2021-03-27 15:15:08 +0000

    fix getters and setters business.c

--- project_c/src/model/businesses.c
+++ project_c/src/model/businesses.c
@@ -21,64 +21,77 @@ struct business_collection {
   GTree *by_name;
 };
 
+/* Business: builder */
+
+Business create_business(char *business_id, char *name, char *city, char *state,
+                         GPtrArray *categories) {
+  Business new_business = (Business)malloc(sizeof(struct business));
+  *new_business = (struct business){.business_id = g_strdup(business_id),
+                                    .name = g_strdup(name),
+                                    .city = g_strdup(city),
+                                    .state = g_strdup(state),
+                                    .categories = categories};
+  return new_business;
+}
+
 /* Business: getters and setters */
 
-char const *getBusinessId(Business self) {
+char *get_business_id(Business self) {
   if (self)
     return self->business_id;
   else
     return NULL;
 }
 
-void *setBusinessId(Business self, char *business_id) {
+void set_business_id(Business self, char *business_id) {
   if (self)
-    strcpy(self->business_id, business_id);
+    self->business_id = g_strdup(business_id);
 }
 
-char const *getName(Business self) {
+char *get_name(Business self) {
   if (self)
     return self->name;
   else
     return NULL;
 }
 
-void *setName(Business self, char *name) {
+void set_name(Business self, char *name) {
   if (self)
-    strcpy(self->name, name);
+    self->name = g_strdup(name);
 }
 
-char const *getCity(Business self) {
+char *get_city(Business self) {
   if (self)
     return self->city;
   else
     return NULL;
 }
 
-void *setCity(Business self, char *city) {
+void set_city(Business self, char *city) {
   if (self)
-    strcpy(self->city, city);
+    self->city = g_strdup(city);
 }
 
-char const *getState(Business self) {
+char *get_state(Business self) {
   if (self)
     return self->state;
   else
-    NULL;
+    return NULL;
 }
 
-void *setState(Business self, char *state) {
+void set_state(Business self, char *state) {
   if (self)
-    strcpy(self->state, state);
+    self->state = g_strdup(state);
 }
 
-GPtrArray const *getCategories(Business self) {
+GPtrArray *get_categories(Business self) {
   if (self)
     return self->categories;
   else
-    NULL;
+    return NULL;
 }
 
-void *setCategories(Business self, GPtrArray *categories) {
+void set_categories(Business self, GPtrArray *categories) {
   if (self) {
     // ver melhor
     if (g_ptr_array_free(self->categories, TRUE) != NULL) {
@@ -88,16 +101,45 @@ void *setCategories(Business self, GPtrArray *categories) {
   }
 }
 
+/* Business: free */
+
+void free_business(Business self) {
+  if (self) {
+    free(self->business_id);
+    free(self->name);
+    free(self->city);
+    free(self->state);
+    g_ptr_array_free(self->categories, TRUE);
+    // ??
+    free(self);
+  }
+}
+
+/* BusinessCollection: builder */
+BusinessCollection create_business_collection(GArray *businesses,
+                                              GHashTable *by_id,
+                                              GHashTable *by_city,
+                                              GTree *by_name) {
+  BusinessCollection new_business_collection =
+      (BusinessCollection)malloc(sizeof(struct business_collection));
+  *new_business_collection =
+      (struct business_collection){.businesses = businesses,
+                                   .by_id = by_id,
+                                   .by_city = by_city,
+                                   .by_name = by_name};
+  return new_business_collection;
+}
+
 /* business_collection: getters and setters */
 
-GArray const *getBusinesses(BusinessCollection self) {
+GArray *get_businesses(BusinessCollection self) {
   if (self)
     return self->businesses;
   else
-    NULL;
+    return NULL;
 }
 
-void *setBusinesses(BusinessCollection self, GArray *businesses) {
+void set_businesses(BusinessCollection self, GArray *businesses) {
   if (self) {
     if (g_array_get_element_size(self->businesses) > 0) {
       g_array_free(self->businesses, TRUE);
@@ -106,61 +148,76 @@ void *setBusinesses(BusinessCollection self, GArray *businesses) {
   }
 }
 
-void *addBusiness(BusinessCollection self, Business elem) {
+void add_business(BusinessCollection self, Business elem) {
   // adicionar no fim devido as Hashs nas reviews
   // ???
   if (self)
     g_array_prepend_val(self->businesses, elem);
 }
 
-GHashTable const *getBy_id(BusinessCollection self) {
+GHashTable *get_by_id(BusinessCollection self) {
   if (self)
     return self->by_id;
   else
-    NULL;
+    return NULL;
 }
 
-void *setBy_id(BusinessCollection self, GHashTable *by_id) {
+void set_by_id(BusinessCollection self, GHashTable *by_id) {
   // libertar memoria ou nao
   if (self)
     self->by_id = by_id;
 }
 
-void *addBy_id(BusinessCollection self, gpointer elem) {
+void add_by_id(BusinessCollection self, gpointer elem) {
   if (self)
     g_hash_table_add(self->by_id, elem);
 }
 
-GHashTable const *getBy_city(BusinessCollection self) {
+GHashTable *get_by_city(BusinessCollection self) {
   if (self)
     return self->by_city;
   else
     return NULL;
 }
 
-void *setBy_city(BusinessCollection self, GHashTable *by_city) {
+void set_by_city(BusinessCollection self, GHashTable *by_city) {
   // libertar memoria ou nao
   if (self)
     self->by_city = by_city;
 }
 
-void *addBy_city(BusinessCollection self, gpointer elem) {
+void add_by_city(BusinessCollection self, gpointer elem) {
   if (self)
     g_hash_table_add(self->by_city, elem);
 }
 
-GTree const *getBy_name(BusinessCollection self) {
+GTree *get_by_name(BusinessCollection self) {
   if (self)
     return self->by_name;
+  else
+    return NULL;
 }
 
-void *setBy_name(BusinessCollection self, GTree *by_name) {
+void set_by_name(BusinessCollection self, GTree *by_name) {
   if (self) {
     self->by_name = by_name;
   }
 }
 
-void *addBy_name(BusinessCollection self, gpointer key, gpointer value) {
+void add_by_name(BusinessCollection self, gpointer key, gpointer value) {
   if (self)
     g_tree_insert(self->by_name, key, value);
+}
+
+/* BusinessCollection: free */
+
+void free_business_collection(BusinessCollection self) {
+  if (self) {
+    g_array_free(self->businesses, TRUE);
+    g_hash_table_destroy(self->by_id);
+    g_hash_table_destroy(self->by_city);
+    g_tree_destroy(self->by_name);
+    // ???
+    free(self);
+  }
 }
--- project_c/src/model/businesses.h
+++ project_c/src/model/businesses.h
@@ -1,7 +1,40 @@
 #ifndef BUSINESSES_H
 #define BUSINESSES_H
 
+#include <glib.h>
+
 typedef struct business *Business;
 typedef struct business_collection *BusinessCollection;
 
+Business create_business(char *business_id, char *name, char *city, char *state,
+                         GPtrArray *categories);
+char *get_business_id(Business self);
+void set_business_id(Business self, char *business_id);
+char *get_name(Business self);
+void set_name(Business self, char *name);
+char *get_city(Business self);
+void set_city(Business self, char *city);
+char *get_state(Business self);
+void set_state(Business self, char *state);
+GPtrArray *get_categories(Business self);
+void set_categories(Business self, GPtrArray *categories);
+void free_business(Business self);
+BusinessCollection create_business_collection(GArray *businesses,
+                                              GHashTable *by_id,
+                                              GHashTable *by_city,
+                                              GTree *by_name);
+GArray *get_businesses(BusinessCollection self);
+void set_businesses(BusinessCollection self, GArray *businesses);
+void add_business(BusinessCollection self, Business elem);
+GHashTable *get_by_id(BusinessCollection self);
+void set_by_id(BusinessCollection self, GHashTable *by_id);
+void add_by_id(BusinessCollection self, gpointer elem);
+GHashTable *get_by_city(BusinessCollection self);
+void set_by_city(BusinessCollection self, GHashTable *by_city);
+void add_by_city(BusinessCollection self, gpointer elem);
+GTree *get_by_name(BusinessCollection self);
+void set_by_name(BusinessCollection self, GTree *by_name);
+
+void add_by_name(BusinessCollection self, gpointer key, gpointer value);
+void free_business_collection(BusinessCollection self);
 #endif
