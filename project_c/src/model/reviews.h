eab8be1237e08e0fb7726e9676f5b66c50eece90
Matide Bravo <matildeopbravo@gmail.com>
2021-03-30 21:44:43 +0100

    Changed reviews structure and added new makefile to fix issues with readline

--- project_c/src/model/reviews.h
+++ project_c/src/model/reviews.h
@@ -1,25 +1,33 @@
 #ifndef REVIEWS_H
 #define REVIEWS_H
 
-#include "businesses.h"
-#include "users.h"
 #include <glib.h>
 
-typedef struct review *Review;
-typedef struct review_collection *ReviewCollection;
+#include "businesses.h"
+#include "users.h"
 
-Review create_review(char *review_id, char *user_id, char *business_id,
-                     float stars, int useful, int funny, int cool,
-                     GDateTime *date, char *text);
+typedef struct review* Review;
+typedef struct review_collection* ReviewCollection;
 
-char *get_review_id(Review self);
-void set_review_id(Review self, char *review_id);
+Review create_review(
+    char* review_id,
+    char* user_id,
+    char* business_id,
+    float stars,
+    int useful,
+    int funny,
+    int cool,
+    char* date,
+    char* text);
 
-char *get_review_user_id(Review self);
-void set_review_user_id(Review self, char *user_id);
+char* get_review_id(Review self);
+void set_review_id(Review self, char* review_id);
 
-char *get_review_business_id(Review self);
-void set_review_business_id(Review self, char *business_id);
+char* get_review_user_id(Review self);
+void set_review_user_id(Review self, char* user_id);
+
+char* get_review_business_id(Review self);
+void set_review_business_id(Review self, char* business_id);
 
 float get_review_stars(Review self);
 void set_review_stars(Review self, float stars);
@@ -33,33 +41,35 @@ void set_review_funny(Review self, int funny);
 int get_review_cool(Review self);
 void set_review_cool(Review self, int cool);
 
-GDateTime *get_review_date(Review self);
-void set_review_date(Review self, GDateTime *date);
+char* get_review_date(Review self);
+void set_review_date(Review self, char* date);
 
-char *get_review_text(Review self);
-void set_review_text(Review self, char *text);
+char* get_review_text(Review self);
+void set_review_text(Review self, char* text);
 
 void free_review(Review self);
 
-ReviewCollection create_review_collection(GArray *reviews, GHashTable *by_id,
-                                          GHashTable *by_user,
-                                          GHashTable *by_business);
+ReviewCollection create_review_collection(
+    GPtrArray* reviews,
+    GHashTable* by_id,
+    GHashTable* by_user,
+    GHashTable* by_business);
 
-GArray *get_reviewCollection_reviews(ReviewCollection self);
-void set_reviewCollection_reviews(ReviewCollection self, GArray *reviews);
+GPtrArray* get_reviewCollection_reviews(ReviewCollection self);
+void set_reviewCollection_reviews(ReviewCollection self, GPtrArray* reviews);
 
-GHashTable *get_reviewCollection_by_id(ReviewCollection self);
-User get_reviewCollection_user_by_id(ReviewCollection self, char *id);
-void set_reviewCollection_by_id(ReviewCollection self, GHashTable *by_id);
+GHashTable* get_reviewCollection_by_id(ReviewCollection self);
+User get_reviewCollection_user_by_id(ReviewCollection self, char* id);
+void set_reviewCollection_by_id(ReviewCollection self, GHashTable* by_id);
 
-GHashTable *get_reviewCollection_by_user(ReviewCollection self);
-User get_reviewCollection_user_by_user(ReviewCollection self, char *id);
-void set_reviewCollection_by_user(ReviewCollection self, GHashTable *by_user);
+GHashTable* get_reviewCollection_by_user(ReviewCollection self);
+User get_reviewCollection_user_by_user(ReviewCollection self, char* id);
+void set_reviewCollection_by_user(ReviewCollection self, GHashTable* by_user);
 
-GHashTable *get_reviewCollection_by_business(ReviewCollection self);
-Business get_reviewCollection_business_by_business(ReviewCollection self,
-                                                   char *id);
-void set_reviewCollection_by_business(ReviewCollection self,
-                                      GHashTable *by_business);
+GHashTable* get_reviewCollection_by_business(ReviewCollection self);
+Business get_reviewCollection_business_by_business(
+    ReviewCollection self, char* id);
+void set_reviewCollection_by_business(
+    ReviewCollection self, GHashTable* by_business);
 void free_reviewCollection(ReviewCollection self);
 #endif
