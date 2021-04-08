80ad53b13f411d6277da806f71c1ebc5304f967d
Matide Bravo <matildeopbravo@gmail.com>
2021-04-08 11:32:20 +0100

    Replaced strndup for g_strdndup

--- project_c/src/view/parsing.c
+++ project_c/src/view/parsing.c
@@ -1,374 +1,382 @@
 #include "parsing.h"
-#include "colors.h"
-#include <stdio.h>
+
 #include <glib.h>
+#include <stdio.h>
 #include <stdlib.h>
 
+#include "colors.h"
+
 // Verifica se a string é uma referência a uma variável
-int is_variable_reference(const char *string) {
-  int i = 0;
+int is_variable_reference(const char* string) {
+    int i = 0;
 
-  // Passa à frente todos os espaços
-  for (; string[i] && string[i] == ' '; i++);
+    // Passa à frente todos os espaços
+    for (; string[i] && string[i] == ' '; i++)
+        ;
 
-  // É um comando
-  if (string[i] == ':')
-    return 0;
+    // É um comando
+    if (string[i] == ':') return 0;
 
-  for (; string[i] && string[i] != ' '; i++) {
-    if (string[i] == '(')
-      return 0;
-  }
+    for (; string[i] && string[i] != ' '; i++) {
+        if (string[i] == '(') return 0;
+    }
 
-  return 1;
+    return 1;
 }
 
-GArray *split_args(char *string) {
-  char is_function = 0;
-  GArray *args = g_array_new(FALSE, FALSE, sizeof (char *));
-  // 1. retiramos whitespace do inicio da string
-  g_strchug(string);
+GArray* split_args(char* string) {
+    char is_function = 0;
+    GArray* args = g_array_new(FALSE, FALSE, sizeof(char*));
+    // 1. retiramos whitespace do inicio da string
+    g_strchug(string);
 
-  // 2. Verificamos se é um comando (começa por ':' e não tem parênteses) ou uma função
-  if (*string != ':') {
-    is_function = 1;
+    // 2. Verificamos se é um comando (começa por ':' e não tem parênteses) ou
+    // uma função
+    if (*string != ':') {
+        is_function = 1;
 
-    string = g_strchomp(strtok(string, "("));
-  } else {
-    string = strtok(string, " ");
-  }
-  g_array_append_val(args, string);
+        string = g_strchomp(strtok(string, "("));
+    }
+    else {
+        string = strtok(string, " ");
+    }
+    g_array_append_val(args, string);
 
-  if (is_function) {
-  }
+    if (is_function) {
+    }
 
-  return NULL;
-}
-
-// Duplica n caracteres de uma string
-char *strndup(const char *s, int n) {
-  char *copy = malloc(n + 1);
-  int i;
-
-  // TODO usar memcpy ou algo do género: memcpy pode ser compilado como uma
-  // única instrução, é consideravelmente mais rápido
-  for (i = 0; s[i] && i < n; i++) {
-    copy[i] = s[i];
-  }
-
-  copy[i] = '\0';
-
-  return copy;
-}
-
-const char *identify(const char *string, Token *dest) {
-  const char *s;
-  int i;
-
-  // Vamos passar à frente qualquer espaço
-  for (s = string; *s && *s == ' '; s++);
-
-  dest->position = s - string;
-
-  if (*s == '\0') {
-    dest->type = TOK_FINISH;
-    dest->text = NULL;
     return NULL;
-  } else if (*s == '=') {
-    dest->type = TOK_EQUALS;
-    dest->text = "=";
-    return s + 1;
-  } else if (*s == ',') {
-    dest->type = TOK_COMMA;
-    dest->text = ",";
-    return s + 1;
-  } else if (*s == '(') {
-    dest->type = TOK_OPAREN;
-    dest->text = "("; 
-    return s + 1;
-  } else if (*s == ')') {
-    dest->type = TOK_CPAREN;
-    dest->text = ")";
-    return s + 1;
-  } else if (*s == ';') {
-    dest->type = TOK_SEMICOLON;
-    dest->text = ";";
-    return s + 1;
-  } else if (*s >= '0' && *s <= '9') {
-    // Encontrámos um número. Vamos ler até encontrarmos algo diferente
-    // TODO isto tem de lidar com decimais...
-    for (i = 0; s[i] && s[i] >= '0' && s[i] <= '9'; i++);
-    dest->type = TOK_NUMBER;
-    dest->text = strndup(s, i);
-
-    return s + i;
-  } else if (*s == '"') {
-    dest->type = TOK_STRING;
-    
-    // Vamos implementar escaping de strings, simplesmente ignorando um \ e adicionando o caracter a seguir
-    
-    for (i = 1; s[i] && s[i] != '"'; i++) {
-      if (s[i] == '\\')
-        i++;
-    }
-
-    if (s[i] == '"')
-      i++;
-
-    dest->text = strndup(s, i);
-
-    return s + i;
-  } else {
-    dest->type = TOK_NAME;
-
-    // Validação enorme....
-    // Com isto emojis são nomes de variáveis válidos, fun fact
-    for (i = 0; s[i] && s[i] != '"' && s[i] != ' ' && s[i] != '\t' && s[i] != '\n' && s[i] != '=' && s[i] != '(' && s[i] != ')' && s[i] != ';' && s[i] != ','; i++);
-
-    dest->text = strndup(s, i);
-    return s + i;
-  }
 }
 
-Token *split_line(const char *string) {
-  const char *cur_line = string;
-  // Porquê 6? É o que uma chamada de função básica vai utilizar: nome, dois parênteses, um argumento, ponto e virgula e o final
-  Token *line = malloc(sizeof (Token) * 6);
-  int size = 6;
-  int i = 0;
+const char* identify(const char* string, Token* dest) {
+    const char* s;
+    int i;
 
-  while (1) {
-    const char *prev = cur_line;
-    cur_line = identify(cur_line, &line[i]);
-    line[i].position += prev - string;
-    if (cur_line == NULL)
-      break;
-    i++;
-    if (i >= size) {
-      size *= 2;
-      line = realloc(line, size * sizeof (Token));
+    // Vamos passar à frente qualquer espaço
+    for (s = string; *s && *s == ' '; s++)
+        ;
+
+    dest->position = s - string;
+
+    if (*s == '\0') {
+        dest->type = TOK_FINISH;
+        dest->text = NULL;
+        return NULL;
     }
-  }
+    else if (*s == '=') {
+        dest->type = TOK_EQUALS;
+        dest->text = "=";
+        return s + 1;
+    }
+    else if (*s == ',') {
+        dest->type = TOK_COMMA;
+        dest->text = ",";
+        return s + 1;
+    }
+    else if (*s == '(') {
+        dest->type = TOK_OPAREN;
+        dest->text = "(";
+        return s + 1;
+    }
+    else if (*s == ')') {
+        dest->type = TOK_CPAREN;
+        dest->text = ")";
+        return s + 1;
+    }
+    else if (*s == ';') {
+        dest->type = TOK_SEMICOLON;
+        dest->text = ";";
+        return s + 1;
+    }
+    else if (*s >= '0' && *s <= '9') {
+        // Encontrámos um número. Vamos ler até encontrarmos algo diferente
+        // TODO isto tem de lidar com decimais...
+        for (i = 0; s[i] && s[i] >= '0' && s[i] <= '9'; i++)
+            ;
+        dest->type = TOK_NUMBER;
+        dest->text = g_strndup(s, i);
 
-  return line;
+        return s + i;
+    }
+    else if (*s == '"') {
+        dest->type = TOK_STRING;
+
+        // Vamos implementar escaping de strings, simplesmente ignorando um \ e
+        // adicionando o caracter a seguir
+
+        for (i = 1; s[i] && s[i] != '"'; i++) {
+            if (s[i] == '\\') i++;
+        }
+
+        if (s[i] == '"') i++;
+
+        dest->text = g_strndup(s, i);
+
+        return s + i;
+    }
+    else {
+        dest->type = TOK_NAME;
+
+        // Validação enorme....
+        // Com isto emojis são nomes de variáveis válidos, fun fact
+        for (i = 0; s[i] && s[i] != '"' && s[i] != ' ' && s[i] != '\t' &&
+                    s[i] != '\n' && s[i] != '=' && s[i] != '(' && s[i] != ')' &&
+                    s[i] != ';' && s[i] != ',';
+             i++)
+            ;
+
+        dest->text = g_strndup(s, i);
+        return s + i;
+    }
+}
+
+Token* split_line(const char* string) {
+    const char* cur_line = string;
+    // Porquê 6? É o que uma chamada de função básica vai utilizar: nome, dois
+    // parênteses, um argumento, ponto e virgula e o final
+    Token* line = malloc(sizeof(Token) * 6);
+    int size = 6;
+    int i = 0;
+
+    while (1) {
+        const char* prev = cur_line;
+        cur_line = identify(cur_line, &line[i]);
+        line[i].position += prev - string;
+        if (cur_line == NULL) break;
+        i++;
+        if (i >= size) {
+            size *= 2;
+            line = realloc(line, size * sizeof(Token));
+        }
+    }
+
+    return line;
 }
 
 // Devolve um syntax error
-SyntaxError *syntax_error(const char *expected, const Token *token) {
-  SyntaxError *err = malloc(sizeof(SyntaxError));
-  err->expected = expected;
-  err->token = token;
-  return err;
+SyntaxError* syntax_error(const char* expected, const Token* token) {
+    SyntaxError* err = malloc(sizeof(SyntaxError));
+    err->expected = expected;
+    err->token = token;
+    return err;
 }
 
-void print_error(SyntaxError *error, const char *line) {
-  fprintf(stderr, BOLD FG_RED "Syntax error" RESET_ALL " on position %d. Expected " BOLD "%s" RESET_ALL ", but found " BOLD "'%s'" RESET_ALL ".\n", error->token->position, error->expected, token_text(error->token));
+void print_error(SyntaxError* error, const char* line) {
+    fprintf(
+        stderr,
+        BOLD FG_RED "Syntax error" RESET_ALL " on position %d. Expected " BOLD
+                    "%s" RESET_ALL ", but found " BOLD "'%s'" RESET_ALL ".\n",
+        error->token->position,
+        error->expected,
+        token_text(error->token));
 }
 
-GArray *parse_line(const char *string) {
-  // 1. Criar parsers diferentes, um que lê uma chamada de função
-  // (prioridade 1), outro que lê uma variável (prioridade 2), outro
-  // que lê um valor (prioridade 3).
+GArray* parse_line(const char* string) {
+    // 1. Criar parsers diferentes, um que lê uma chamada de função
+    // (prioridade 1), outro que lê uma variável (prioridade 2), outro
+    // que lê um valor (prioridade 3).
 
-  return NULL;
+    return NULL;
 }
 
 // Devolve o token em que deixou de conseguir ler
-SyntaxError *parse_function(const Token *tokens, AST *node, int *consumed) {
-  int tok = 0;
-  FunctionCall *call;
-  
-  if (tokens[tok].type == TOK_NAME) {
-    call = malloc(sizeof(FunctionCall));
-    call->function_name = tokens[tok].text;
+SyntaxError* parse_function(const Token* tokens, AST* node, int* consumed) {
+    int tok = 0;
+    FunctionCall* call;
+
+    if (tokens[tok].type == TOK_NAME) {
+        call = malloc(sizeof(FunctionCall));
+        call->function_name = tokens[tok].text;
+        tok++;
+    }
+    else {
+        return syntax_error("a function name", &tokens[tok]);
+    }
+
+    if (tokens[tok].type != TOK_OPAREN) {
+        free(call);
+        return syntax_error("'('", &tokens[tok]);
+    }
+
     tok++;
-  } else {
-    return syntax_error("a function name", &tokens[tok]);
-  }
 
-  if (tokens[tok].type != TOK_OPAREN) {
-    free(call);
-    return syntax_error("'('", &tokens[tok]);
-  }
+    call->args = g_array_new(FALSE, FALSE, sizeof(AST));
 
-  tok++;
+    while (tokens[tok].type != TOK_CPAREN) {
+        int consumed = 0;
+        AST node;
+        SyntaxError* e = parse_expression(&tokens[tok], &node, &consumed);
 
-  call->args = g_array_new(FALSE, FALSE, sizeof (AST));
-  
-  while (tokens[tok].type != TOK_CPAREN) {
-    int consumed = 0;
-    AST node;
-    SyntaxError *e = parse_expression(&tokens[tok], &node, &consumed);
+        if (e) {
+            g_array_free(call->args, TRUE);
+            free(call);
+            return e;
+        }
 
-    if (e) {
-      g_array_free(call->args, TRUE);
-      free(call);
-      return e;
+        tok += consumed;
+
+        if (tokens[tok].type != TOK_CPAREN && tokens[tok].type != TOK_COMMA) {
+            g_array_free(call->args, TRUE);
+            free(call);
+            return syntax_error("',' or ')'", &tokens[tok]);
+        }
+
+        g_array_append_val(call->args, node);
+        if (tokens[tok].type == TOK_COMMA) tok++;
     }
 
-    tok += consumed;
-
-    if (tokens[tok].type != TOK_CPAREN && tokens[tok].type != TOK_COMMA) {
-      g_array_free(call->args, TRUE);
-      free(call);
-      return syntax_error("',' or ')'", &tokens[tok]);
-    }
-
-    g_array_append_val(call->args, node);
-    if (tokens[tok].type == TOK_COMMA)
-      tok++;
-  }
-
-  tok++;
-  *consumed = tok;
-  node->type = AST_FUNCTIONCALL;
-  node->value.function = call;
-
-  return NULL;
-}
-
-
-SyntaxError *parse_assignment(const Token *tokens, AST *node, int *consumed) {
-  int tok = 0;
-  VarAssignment *assignment;
-
-  if (tokens[tok].type == TOK_NAME) {
-    assignment = malloc(sizeof(VarAssignment));
-    assignment->variable = tokens[tok].text;
     tok++;
-  } else {
-    return syntax_error("a name", tokens);
-  }
+    *consumed = tok;
+    node->type = AST_FUNCTIONCALL;
+    node->value.function = call;
 
-  if (tokens[tok].type != TOK_EQUALS) {
-    free(assignment);
-    return syntax_error("'='", &tokens[tok]);
-  }
-
-  tok++;
-
-  int c = 0;
-  AST *n = malloc(sizeof(AST));
-  SyntaxError *e = parse_expression(&tokens[tok], n, &c);
-
-  if (e) {
-    free(assignment);
-    free(n);
-    return e;
-  }
-
-  tok += c;
-
-  *consumed = tok;
-  assignment->value = n;
-
-  node->type = AST_ASSIGNMENT;
-  node->value.assignment = assignment;
-
-  return NULL;
+    return NULL;
 }
 
-SyntaxError *parse_expression(const Token *tokens, AST *node, int *consumed) {
-  if (tokens->type == TOK_NUMBER) {
-    node->type = AST_NUMBER;
-    node->value.number = atoi(tokens->text);
-    *consumed = 1;
-    return NULL;
-  } else if (tokens->type == TOK_NAME) {
-    // Vamos tentar ler uma função, se não é uma variável
-    SyntaxError *e = parse_function(tokens, node, consumed);
+SyntaxError* parse_assignment(const Token* tokens, AST* node, int* consumed) {
+    int tok = 0;
+    VarAssignment* assignment;
 
-    if (e) {
-      free(e);
-      node->type = AST_VARIABLE;
-      node->value.variable = tokens->text;
-      *consumed = 1;
+    if (tokens[tok].type == TOK_NAME) {
+        assignment = malloc(sizeof(VarAssignment));
+        assignment->variable = tokens[tok].text;
+        tok++;
+    }
+    else {
+        return syntax_error("a name", tokens);
     }
 
-    return NULL;
-  } else if (tokens->type == TOK_STRING) {
-    // TODO string escaping
-    node->type = AST_STRING;
-    node->value.string = tokens->text;
-    *consumed = 1;
-    return NULL;
-  }
+    if (tokens[tok].type != TOK_EQUALS) {
+        free(assignment);
+        return syntax_error("'='", &tokens[tok]);
+    }
 
-  return syntax_error("an expression", tokens);
-}
+    tok++;
 
-SyntaxError *parse_statement(const Token *tokens, AST *node, int *consumed) {
-  SyntaxError *e = parse_function(tokens, node, consumed);
-
-  if (e) {
-    free(e);
-    e = parse_assignment(tokens, node, consumed);
+    int c = 0;
+    AST* n = malloc(sizeof(AST));
+    SyntaxError* e = parse_expression(&tokens[tok], n, &c);
 
     if (e) {
-      free(e);
-      return syntax_error("a statement", tokens);
-    } 
-  }
+        free(assignment);
+        free(n);
+        return e;
+    }
 
-  if (tokens[*consumed].type != TOK_SEMICOLON) {
-    free(e);
-    return syntax_error("';'", &tokens[*consumed]);
-  }
+    tok += c;
 
-  *consumed += 1;
+    *consumed = tok;
+    assignment->value = n;
 
-  return NULL;
+    node->type = AST_ASSIGNMENT;
+    node->value.assignment = assignment;
+
+    return NULL;
 }
 
-const char *token_text(const Token *token) {
-  switch (token->type) {
-    case TOK_NAME:
-    case TOK_STRING:
-    case TOK_NUMBER:
-      return token->text;
-    case TOK_EQUALS:
-      return "=";
-    case TOK_COMMA:
-      return ",";
-    case TOK_OPAREN:
-      return "(";
-    case TOK_CPAREN:
-      return ")";
-    case TOK_SEMICOLON:
-      return ";";
-    default:
-      return "";
-  }
+SyntaxError* parse_expression(const Token* tokens, AST* node, int* consumed) {
+    if (tokens->type == TOK_NUMBER) {
+        node->type = AST_NUMBER;
+        node->value.number = atoi(tokens->text);
+        *consumed = 1;
+        return NULL;
+    }
+    else if (tokens->type == TOK_NAME) {
+        // Vamos tentar ler uma função, se não é uma variável
+        SyntaxError* e = parse_function(tokens, node, consumed);
+
+        if (e) {
+            free(e);
+            node->type = AST_VARIABLE;
+            node->value.variable = tokens->text;
+            *consumed = 1;
+        }
+
+        return NULL;
+    }
+    else if (tokens->type == TOK_STRING) {
+        // TODO string escaping
+        node->type = AST_STRING;
+        node->value.string = tokens->text;
+        *consumed = 1;
+        return NULL;
+    }
+
+    return syntax_error("an expression", tokens);
 }
 
-void print_element(Token *e) {
-  switch (e->type) {
-    case TOK_NAME:
-      printf("NAME(%s)", e->text);
-      break;
-    case TOK_STRING:
-      printf("STRING(%s)", e->text);
-      break;
-    case TOK_NUMBER:
-      printf("NUMBER(%s)", e->text);
-      break;
-    case TOK_EQUALS:
-      printf("=");
-      break;
-    case TOK_COMMA:
-      printf(",");
-      break;
-    case TOK_OPAREN:
-      printf("(");
-      break;
-    case TOK_CPAREN:
-      printf(")");
-      break;
-    case TOK_SEMICOLON:
-      printf(";");
-      break;
-    case TOK_FINISH:
-      printf("FINISH");
-      break;
-  }
+SyntaxError* parse_statement(const Token* tokens, AST* node, int* consumed) {
+    SyntaxError* e = parse_function(tokens, node, consumed);
+
+    if (e) {
+        free(e);
+        e = parse_assignment(tokens, node, consumed);
+
+        if (e) {
+            free(e);
+            return syntax_error("a statement", tokens);
+        }
+    }
+
+    if (tokens[*consumed].type != TOK_SEMICOLON) {
+        free(e);
+        return syntax_error("';'", &tokens[*consumed]);
+    }
+
+    *consumed += 1;
+
+    return NULL;
+}
+
+const char* token_text(const Token* token) {
+    switch (token->type) {
+        case TOK_NAME:
+        case TOK_STRING:
+        case TOK_NUMBER:
+            return token->text;
+        case TOK_EQUALS:
+            return "=";
+        case TOK_COMMA:
+            return ",";
+        case TOK_OPAREN:
+            return "(";
+        case TOK_CPAREN:
+            return ")";
+        case TOK_SEMICOLON:
+            return ";";
+        default:
+            return "";
+    }
+}
+
+void print_element(Token* e) {
+    switch (e->type) {
+        case TOK_NAME:
+            printf("NAME(%s)", e->text);
+            break;
+        case TOK_STRING:
+            printf("STRING(%s)", e->text);
+            break;
+        case TOK_NUMBER:
+            printf("NUMBER(%s)", e->text);
+            break;
+        case TOK_EQUALS:
+            printf("=");
+            break;
+        case TOK_COMMA:
+            printf(",");
+            break;
+        case TOK_OPAREN:
+            printf("(");
+            break;
+        case TOK_CPAREN:
+            printf(")");
+            break;
+        case TOK_SEMICOLON:
+            printf(";");
+            break;
+        case TOK_FINISH:
+            printf("FINISH");
+            break;
+    }
 }
--- project_c/src/view/parsing.h
+++ project_c/src/view/parsing.h
@@ -2,77 +2,78 @@
 #define PARSING_H
 #include <glib.h>
 
-// Um elemento pode ser um nome, um igual, uma virgula, um número, parênteses ou um ponto e virgula
+// Um elemento pode ser um nome, um igual, uma virgula, um número, parênteses ou
+// um ponto e virgula
 typedef enum token_type {
-  TOK_NAME,        // e.g. x, avg, fromCSV
-  TOK_STRING,      // e.g. "business.csv"
-  TOK_NUMBER,      // e.g. 0, 15
-  TOK_EQUALS,      // =
-  TOK_COMMA,       // ,
-  TOK_OPAREN,      // (
-  TOK_CPAREN,      // )
-  TOK_SEMICOLON,   // ;
-  TOK_FINISH       // Não temos mais elementos
+    TOK_NAME,       // e.g. x, avg, fromCSV
+    TOK_STRING,     // e.g. "business.csv"
+    TOK_NUMBER,     // e.g. 0, 15
+    TOK_EQUALS,     // =
+    TOK_COMMA,      // ,
+    TOK_OPAREN,     // (
+    TOK_CPAREN,     // )
+    TOK_SEMICOLON,  // ;
+    TOK_FINISH      // Não temos mais elementos
 } TokenType;
 
 typedef struct token {
-  TokenType type;
-  char *text;
-  unsigned int position;
+    TokenType type;
+    char* text;
+    unsigned int position;
 } Token;
 
 // Com isto podemos converter e.g. data = fromCSV("reviews.csv"); em:
-// { TOK_NAME("data"), TOK_EQUALS, TOK_NAME("fromCSV"), TOK_OPAREN, TOK_STRING("reviews.csv"), TOK_CPAREN, TOK_SEMICOLON, TOK_FINISH }
+// { TOK_NAME("data"), TOK_EQUALS, TOK_NAME("fromCSV"), TOK_OPAREN,
+// TOK_STRING("reviews.csv"), TOK_CPAREN, TOK_SEMICOLON, TOK_FINISH }
 
-Token *split_line(const char *string);
-char *strndup(const char *s, int n);
-const char *identify(const char *string, Token *dest);
+Token* split_line(const char* string);
+const char* identify(const char* string, Token* dest);
 
 typedef enum ast_type {
-  AST_FUNCTIONCALL,
-  AST_ASSIGNMENT,
-  AST_VARIABLE,
-  AST_NUMBER,
-  AST_STRING
+    AST_FUNCTIONCALL,
+    AST_ASSIGNMENT,
+    AST_VARIABLE,
+    AST_NUMBER,
+    AST_STRING
 } ASTType;
 
 typedef struct function_call {
-  char *function_name;
-  GArray *args;
+    char* function_name;
+    GArray* args;
 } FunctionCall;
 
 typedef struct var_assignment {
-  char *variable;
-  struct ast *value;
+    char* variable;
+    struct ast* value;
 } VarAssignment;
 
 typedef struct ast {
-  ASTType type;
-  union {
-    FunctionCall *function;
-    VarAssignment *assignment;
-    char *variable;
-    int number;
-    char *string;
-  } value;
+    ASTType type;
+    union {
+        FunctionCall* function;
+        VarAssignment* assignment;
+        char* variable;
+        int number;
+        char* string;
+    } value;
 } AST;
 
 typedef struct syntax_error {
-  const char *expected;
-  const Token *token;
+    const char* expected;
+    const Token* token;
 } SyntaxError;
 
-SyntaxError *syntax_error(const char *expected, const Token *token);
-void print_error(SyntaxError *error, const char *line);
+SyntaxError* syntax_error(const char* expected, const Token* token);
+void print_error(SyntaxError* error, const char* line);
 
-GArray *parse_line(const char *string);
+GArray* parse_line(const char* string);
 
-SyntaxError *parse_function(const Token *tokens, AST *node, int *consumed);
-SyntaxError *parse_assignment(const Token *tokens, AST *node, int *consumed);
-SyntaxError *parse_expression(const Token *tokens, AST *node, int *consumed);
-SyntaxError *parse_statement(const Token *tokens, AST *node, int *consumed);
+SyntaxError* parse_function(const Token* tokens, AST* node, int* consumed);
+SyntaxError* parse_assignment(const Token* tokens, AST* node, int* consumed);
+SyntaxError* parse_expression(const Token* tokens, AST* node, int* consumed);
+SyntaxError* parse_statement(const Token* tokens, AST* node, int* consumed);
 
-const char *token_text(const Token *token);
-void print_element(Token *e);
+const char* token_text(const Token* token);
+void print_element(Token* e);
 
 #endif
