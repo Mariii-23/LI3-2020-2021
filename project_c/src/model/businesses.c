d4f968f999a1a83cf2dfa704515c70287066a000
Mariii-23 <marianadinis8@hotmail.com>
2021-04-25 14:32:31 +0100

    Fixed erros

--- project_c/src/model/businesses.c
+++ project_c/src/model/businesses.c
@@ -10,197 +10,195 @@
 #include "users.h"
 
 struct business {
-    char* business_id;
-    char* name;
-    char* city;
-    char* state;
-    GPtrArray* categories;
+  char *business_id;
+  char *name;
+  char *city;
+  char *state;
+  GPtrArray *categories;
 };
 
 struct business_collection {
-    GPtrArray* businesses;
-    GHashTable* by_id;
-    PerfectHash by_letter;
+  GHashTable *by_id;
+  PerfectHash by_letter;
 
-    GHashTable* by_state;  // Hash* Id-> Hash* State
-                           // ->GPtrArray* City
+  GHashTable *by_state; // Hash* Id-> Hash* State
+                        // ->GPtrArray* City
 };
 
 /* Business: builder */
 
-Business create_business(
-    char* business_id,
-    char* name,
-    char* city,
-    char* state,
-    GPtrArray* categories) {
-    Business new_business = (Business) malloc(sizeof(struct business));
-    *new_business = (struct business){
-        .business_id = g_strdup(business_id),
-        .name = g_strdup(name),
-        .city = g_strdup(city),
-        .state = g_strdup(state),
-        .categories = g_ptr_array_copy(categories, strdup_copy, NULL)};
-    return new_business;
+Business create_business(char *business_id, char *name, char *city, char *state,
+                         GPtrArray *categories) {
+  Business new_business = (Business)malloc(sizeof(struct business));
+  *new_business = (struct business){
+      .business_id = g_strdup(business_id),
+      .name = g_strdup(name),
+      .city = g_strdup(city),
+      .state = g_strdup(state),
+      .categories = g_ptr_array_copy(categories, strdup_copy, NULL)};
+  return new_business;
 }
 
 /* Business: getters and setters */
 
-char* get_business_id(Business self) {
-    if (self)
-        return g_strdup(self->business_id);
-    else
-        return NULL;
+char *get_business_id(Business self) {
+  if (self)
+    return g_strdup(self->business_id);
+  else
+    return NULL;
 }
 
-char* get_business_name(Business self) {
-    if (self)
-        return g_strdup(self->name);
-    else
-        return NULL;
+char *get_business_name(Business self) {
+  if (self)
+    return g_strdup(self->name);
+  else
+    return NULL;
 }
 
-char* get_business_city(Business self) {
-    if (self)
-        return g_strdup(self->city);
-    else
-        return NULL;
+char *get_business_city(Business self) {
+  if (self)
+    return g_strdup(self->city);
+  else
+    return NULL;
 }
 
-char* get_business_state(Business self) {
-    if (self)
-        return g_strdup(self->state);
-    else
-        return NULL;
+char *get_business_state(Business self) {
+  if (self)
+    return g_strdup(self->state);
+  else
+    return NULL;
 }
 
-GPtrArray* get_business_categories(Business self) {
-    if (self && self->categories)
-        return g_ptr_array_copy(self->categories, strdup_copy, NULL);
-    else
-        return NULL;
+GPtrArray *get_business_categories(Business self) {
+  if (self && self->categories)
+    return g_ptr_array_copy(self->categories, strdup_copy, NULL);
+  else
+    return NULL;
 }
 
 static Business clone_business(Business self) {
 
-    Business new_business = (Business) malloc(sizeof(struct business));
-    *new_business = (struct business){
-        .business_id = get_business_id(self),
-        .name = get_business_name(self),
-        .city = get_business_city(self),
-        .state = get_business_state(self),
-        .categories = get_business_categories(self)};
-    return new_business;
+  Business new_business = (Business)malloc(sizeof(struct business));
+  *new_business =
+      (struct business){.business_id = get_business_id(self),
+                        .name = get_business_name(self),
+                        .city = get_business_city(self),
+                        .state = get_business_state(self),
+                        .categories = get_business_categories(self)};
+  return new_business;
 }
 
 static gpointer g_business_copy(gconstpointer src, gpointer data) {
-    return clone_business((Business) src);
+  return clone_business((Business)src);
 }
 
 /* Business: free */
 
 void free_business(Business self) {
-    if (self) {
-        free(self->business_id);
-        free(self->name);
-        free(self->city);
-        free(self->state);
-        g_ptr_array_free(self->categories, TRUE);
-        free(self);
-    }
+  if (self) {
+    free(self->business_id);
+    free(self->name);
+    free(self->city);
+    free(self->state);
+    g_ptr_array_free(self->categories, TRUE);
+    free(self);
+  }
 }
 
 /* BusinessCollection: builder */
 BusinessCollection create_business_collection() {
-    BusinessCollection new_business_collection =
-        (BusinessCollection) malloc(sizeof(struct business_collection));
-    *new_business_collection = (struct business_collection){
-        .businesses = g_ptr_array_new(),
-        .by_id = g_hash_table_new(g_str_hash, g_str_equal),
-        .by_letter = phf_new(),
-        .by_state = g_hash_table_new(g_str_hash, g_str_equal)};
-    return new_business_collection;
+  BusinessCollection new_business_collection =
+      (BusinessCollection)malloc(sizeof(struct business_collection));
+  *new_business_collection = (struct business_collection){
+      .by_id = g_hash_table_new(g_str_hash, g_str_equal),
+      .by_letter = phf_new(),
+      .by_state = g_hash_table_new(g_str_hash, g_str_equal)};
+  return new_business_collection;
 }
 
-static void add_id_state_city(GHashTable* hash_map, Business business) {
-    if (!hash_map) return;
+static void add_id_state_city(GHashTable *hash_map, Business business) {
+  if (!hash_map)
+    return;
 
-    char* id = business->business_id;
-    char* state = business->state;
-    char* city = business->city;
+  char *id = business->business_id;
+  char *state = business->state;
+  char *city = business->city;
 
-    GHashTable* hash_state = g_hash_table_lookup(hash_map, id);
+  GHashTable *hash_state = g_hash_table_lookup(hash_map, id);
 
-    if (!hash_state) {
-        GHashTable* new_hash_state = g_hash_table_new(g_str_hash, g_str_equal);
-        append_to_value(new_hash_state, state, city);
+  if (!hash_state) {
+    GHashTable *new_hash_state = g_hash_table_new(g_str_hash, g_str_equal);
+    append_to_value(new_hash_state, state, city);
 
-        g_hash_table_insert(hash_map, id, new_hash_state);
-        return;
-    }
+    g_hash_table_insert(hash_map, id, new_hash_state);
+    return;
+  }
 
-    append_to_value(hash_state, state, city);
+  append_to_value(hash_state, state, city);
 }
 
 void add_business(BusinessCollection collection, Business business) {
-    if (!collection || !business) return;
-    Business clone = clone_business(business);
-    g_ptr_array_add(collection->businesses, clone);
-    g_hash_table_insert(collection->by_id, business->business_id, clone);
-    phf_add(collection->by_letter, business->name, clone);
-    add_id_state_city(collection->by_state, clone);
+  if (!collection || !business)
+    return;
+  Business clone = clone_business(business);
+  g_hash_table_insert(collection->by_id, business->business_id, clone);
+  phf_add(collection->by_letter, business->name, clone);
+  add_id_state_city(collection->by_state, clone);
 }
 
-Business get_businessCollection_business_by_id(
-    BusinessCollection self, char* id) {
-    Business new = NULL;
-    if (!self || !id) return new;
-
-    Business aux = g_hash_table_lookup(self->by_id, id);
-    if (aux) new = clone_business(aux);
-
+Business get_businessCollection_business_by_id(BusinessCollection self,
+                                               char *id) {
+  Business new = NULL;
+  if (!self || !id)
     return new;
+
+  Business aux = g_hash_table_lookup(self->by_id, id);
+  if (aux)
+    new = clone_business(aux);
+
+  return new;
 }
 
-GPtrArray* get_businessCollection_business_by_letter(
-    BusinessCollection self, char* name) {
-    GPtrArray* new = NULL;
-    if (!self) return new;
-
-    GPtrArray* aux = phf_lookup(self->by_letter, name);
-    if (aux) new = g_ptr_array_copy(aux, g_business_copy, NULL);
-
+GPtrArray *get_businessCollection_business_by_letter(BusinessCollection self,
+                                                     char *name) {
+  GPtrArray *new = NULL;
+  if (!self)
     return new;
+
+  GPtrArray *aux = phf_lookup(self->by_letter, name);
+  if (aux)
+    new = g_ptr_array_copy(aux, g_business_copy, NULL);
+
+  return new;
 }
 
-GSList* business_id_more_than_one_state(BusinessCollection self) {
-    if (!self) return NULL;
+GSList *business_id_more_than_one_state(BusinessCollection self) {
+  if (!self)
+    return NULL;
 
-    GSList* new_list = g_slist_alloc();
+  GSList *new_list = g_slist_alloc();
 
-    GHashTableIter iter;
-    gpointer key, value;
+  GHashTableIter iter;
+  gpointer key, value;
 
-    g_hash_table_iter_init(&iter, self->by_state);
-    while (g_hash_table_iter_next(&iter, &key, &value)) {
-        if (g_hash_table_size((GHashTable*) value) > 1)
-            new_list = g_slist_prepend(new_list, g_strdup((char*) key));
-    }
+  g_hash_table_iter_init(&iter, self->by_state);
+  while (g_hash_table_iter_next(&iter, &key, &value)) {
+    if (g_hash_table_size((GHashTable *)value) > 1)
+      new_list = g_slist_prepend(new_list, g_strdup((char *)key));
+  }
 
-    return new_list;
+  return new_list;
 }
 
 /* BusinessCollection: free */
 
 void free_businessCollection(BusinessCollection self) {
-    if (self) {
-        g_ptr_array_set_free_func(self->businesses, (void*) free_business);
-        g_ptr_array_free(self->businesses, TRUE);
-        g_hash_table_foreach(self->by_id, free_key, NULL);
-        phf_free(self->by_letter);
-        g_hash_table_destroy(self->by_id);
-        // verificar
-        g_hash_table_destroy(self->by_state);
-        free(self);
-    }
+  if (self) {
+    g_hash_table_foreach(self->by_id, free_key, NULL);
+    phf_free(self->by_letter);
+    g_hash_table_destroy(self->by_id);
+    // verificar
+    g_hash_table_destroy(self->by_state);
+    free(self);
+  }
 }
--- project_c/src/model/reviews.c
+++ project_c/src/model/reviews.c
@@ -10,220 +10,206 @@
 #include "users.h"
 
 struct review {
-    char* review_id;
-    char* user_id;
-    char* business_id;
+  char *review_id;
+  char *user_id;
+  char *business_id;
 
-    float stars;
-    int useful;
-    int funny;
-    int cool;
+  float stars;
+  int useful;
+  int funny;
+  int cool;
 
-    char* date;
-    char* text;
+  char *date;
+  char *text;
 };
 
 struct review_collection {
-    GPtrArray* reviews;
-    GHashTable* by_id;
-    GHashTable* by_user_id;
-    GHashTable* by_business_id;
+  GHashTable *by_id;
+  GHashTable *by_user_id;
+  GHashTable *by_business_id;
 };
 
 /* Review: builder */
-Review create_review(
-    char* review_id,
-    char* user_id,
-    char* business_id,
-    float stars,
-    int useful,
-    int funny,
-    int cool,
-    char* date,
-    char* text) {
-    Review new_review = (Review) malloc(sizeof(struct review));
-    *new_review = (struct review){
-        .review_id = g_strdup(review_id),
-        .user_id = g_strdup(user_id),
-        .business_id = g_strdup(business_id),
-        .stars = stars,
-        .useful = useful,
-        .funny = funny,
-        .cool = cool,
-        .date = g_strdup(date),
-        .text = g_strdup(text)};
-    return new_review;
+Review create_review(char *review_id, char *user_id, char *business_id,
+                     float stars, int useful, int funny, int cool, char *date,
+                     char *text) {
+  Review new_review = (Review)malloc(sizeof(struct review));
+  *new_review = (struct review){.review_id = g_strdup(review_id),
+                                .user_id = g_strdup(user_id),
+                                .business_id = g_strdup(business_id),
+                                .stars = stars,
+                                .useful = useful,
+                                .funny = funny,
+                                .cool = cool,
+                                .date = g_strdup(date),
+                                .text = g_strdup(text)};
+  return new_review;
 }
 
 /* Review: getters and setters */
-char* get_review_id(Review self) {
-    if (self) {
-        return g_strdup(self->review_id);
-    }
-    else
-        return NULL;
+char *get_review_id(Review self) {
+  if (self) {
+    return g_strdup(self->review_id);
+  } else
+    return NULL;
 }
 
-char* get_review_user_id(Review self) {
-    if (self) {
-        return g_strdup(self->user_id);
-    }
-    else
-        return NULL;
+char *get_review_user_id(Review self) {
+  if (self) {
+    return g_strdup(self->user_id);
+  } else
+    return NULL;
 }
 
-char* get_review_business_id(Review self) {
-    if (self) {
-        return g_strdup(self->business_id);
-    }
-    else
-        return NULL;
+char *get_review_business_id(Review self) {
+  if (self) {
+    return g_strdup(self->business_id);
+  } else
+    return NULL;
 }
 
 float get_review_stars(Review self) {
-    if (self) {
-        return self->stars;
-    }
-    else
-        return -1;
+  if (self) {
+    return self->stars;
+  } else
+    return -1;
 }
 
 int get_review_useful(Review self) {
-    if (self) {
-        return self->useful;
-    }
-    else
-        return -1;
+  if (self) {
+    return self->useful;
+  } else
+    return -1;
 }
 
 int get_review_funny(Review self) {
-    if (self) {
-        return self->funny;
-    }
-    else
-        return (-1);
+  if (self) {
+    return self->funny;
+  } else
+    return (-1);
 }
 
 int get_review_cool(Review self) {
-    if (self) {
-        return self->cool;
-    }
-    else
-        return -1;
+  if (self) {
+    return self->cool;
+  } else
+    return -1;
 }
 
-char* get_review_date(Review self) {
-    if (self)
-        return g_strdup(self->date);
-    else
-        return NULL;
+char *get_review_date(Review self) {
+  if (self)
+    return g_strdup(self->date);
+  else
+    return NULL;
 }
 
-char* get_review_text(Review self) {
-    if (self) {
-        return g_strdup(self->text);
-    }
-    else
-        return NULL;
+char *get_review_text(Review self) {
+  if (self) {
+    return g_strdup(self->text);
+  } else
+    return NULL;
 }
 
 static Review clone_review(Review self) {
-    Review new_review = (Review) malloc(sizeof(struct review));
-    *new_review = (struct review){
-        .review_id = g_strdup(self->review_id),
-        .user_id = g_strdup(self->user_id),
-        .business_id = g_strdup(self->business_id),
-        .stars = self->stars,
-        .useful = self->useful,
-        .funny = self->funny,
-        .cool = self->cool,
-        .date = g_strdup(self->date),
-        .text = g_strdup(self->text)};
-    return new_review;
+  Review new_review = (Review)malloc(sizeof(struct review));
+  *new_review = (struct review){.review_id = g_strdup(self->review_id),
+                                .user_id = g_strdup(self->user_id),
+                                .business_id = g_strdup(self->business_id),
+                                .stars = self->stars,
+                                .useful = self->useful,
+                                .funny = self->funny,
+                                .cool = self->cool,
+                                .date = g_strdup(self->date),
+                                .text = g_strdup(self->text)};
+  return new_review;
 }
 
 static gpointer g_review_copy(gconstpointer src, gpointer data) {
-    return clone_review((Review) src);
+  return clone_review((Review)src);
 }
 
 void free_review(Review self) {
-    free(self->review_id);
-    free(self->user_id);
-    free(self->business_id);
-    free(self->date);
-    free(self->text);
-    free(self);
+  free(self->review_id);
+  free(self->user_id);
+  free(self->business_id);
+  free(self->date);
+  free(self->text);
+  free(self);
 }
 
 /* Review Collection */
 /* Review Collection: Builder */
 
 ReviewCollection create_review_collection() {
-    ReviewCollection new_review_collection =
-        (ReviewCollection) malloc(sizeof(struct review_collection));
-    *new_review_collection = (struct review_collection){
-        .reviews = g_ptr_array_new(),
-        .by_id = g_hash_table_new(g_str_hash, g_str_equal),
-        .by_user_id = g_hash_table_new(g_str_hash, g_str_equal),
-        .by_business_id = g_hash_table_new(g_str_hash, g_str_equal)};
-    return new_review_collection;
+  ReviewCollection new_review_collection =
+      (ReviewCollection)malloc(sizeof(struct review_collection));
+  *new_review_collection = (struct review_collection){
+      .by_id = g_hash_table_new(g_str_hash, g_str_equal),
+      .by_user_id = g_hash_table_new(g_str_hash, g_str_equal),
+      .by_business_id = g_hash_table_new(g_str_hash, g_str_equal)};
+  return new_review_collection;
 }
 
 void add_review(ReviewCollection collection, Review review) {
-    if (!collection || !review) return;
+  if (!collection || !review)
+    return;
 
-    Review new = clone_review(review);
-    g_hash_table_insert(collection->by_id, review->review_id, new);
+  Review new = clone_review(review);
+  g_hash_table_insert(collection->by_id, review->review_id, new);
 
-    append_to_value(collection->by_user_id, review->user_id, new);
-    append_to_value(collection->by_business_id, review->business_id, new);
-    g_ptr_array_add(collection->reviews, new);
+  append_to_value(collection->by_user_id, review->user_id, new);
+  append_to_value(collection->by_business_id, review->business_id, new);
+  /* g_ptr_array_add(collection->reviews, new); */
 }
 
-Review get_reviewCollection_review_by_id(ReviewCollection self, char* id) {
-    Review new = NULL;
-    if (!self || !id) return new;
-
-    Review aux = g_hash_table_lookup(self->by_id, id);
-    if (aux) new = clone_review(aux);
-
+Review get_reviewCollection_review_by_id(ReviewCollection self, char *id) {
+  Review new = NULL;
+  if (!self || !id)
     return new;
+
+  Review aux = g_hash_table_lookup(self->by_id, id);
+  if (aux)
+    new = clone_review(aux);
+
+  return new;
 }
 
-int get_number_reviews_by_business(ReviewCollection self, char* business_id) {
-    return get_reviewCollection_review_by_business_id(self, business_id)->len;
+int get_number_reviews_by_business(ReviewCollection self, char *business_id) {
+  return get_reviewCollection_review_by_business_id(self, business_id)->len;
 }
 
-GPtrArray* get_reviewCollection_review_by_user_id(
-    ReviewCollection self, char* id) {
-    GPtrArray* new = NULL;
-    if (!self || !id) return new;
-
-    GPtrArray* aux = g_hash_table_lookup(self->by_user_id, id);
-    if (aux) new = g_ptr_array_copy(aux, g_review_copy, NULL);
+GPtrArray *get_reviewCollection_review_by_user_id(ReviewCollection self,
+                                                  char *id) {
+  GPtrArray *new = NULL;
+  if (!self || !id)
     return new;
+
+  GPtrArray *aux = g_hash_table_lookup(self->by_user_id, id);
+  if (aux)
+    new = g_ptr_array_copy(aux, g_review_copy, NULL);
+  return new;
 }
 
-GPtrArray* get_reviewCollection_review_by_business_id(
-    ReviewCollection self, char* id) {
-    GPtrArray* new = NULL;
-    if (!self || !id) return new;
-
-    GPtrArray* aux = g_hash_table_lookup(self->by_business_id, id);
-    if (aux) new = g_ptr_array_copy(aux, g_review_copy, NULL);
+GPtrArray *get_reviewCollection_review_by_business_id(ReviewCollection self,
+                                                      char *id) {
+  GPtrArray *new = NULL;
+  if (!self || !id)
     return new;
+
+  GPtrArray *aux = g_hash_table_lookup(self->by_business_id, id);
+  if (aux)
+    new = g_ptr_array_copy(aux, g_review_copy, NULL);
+  return new;
 }
 
 void free_reviewCollection(ReviewCollection self) {
-    if (self) {
-        g_ptr_array_set_free_func(self->reviews, (void*) free_review);
-        g_ptr_array_free(self->reviews, TRUE);
-        g_hash_table_foreach(self->by_id, free_key, NULL);
-        g_hash_table_foreach(self->by_user_id, free_key, NULL);
-        g_hash_table_foreach(self->by_business_id, free_key, NULL);
-        g_hash_table_destroy(self->by_id);
-        g_hash_table_destroy(self->by_user_id);
-        g_hash_table_destroy(self->by_business_id);
-        free(self);
-    }
+  if (self) {
+    g_hash_table_foreach(self->by_id, free_key, NULL);
+    g_hash_table_foreach(self->by_user_id, free_key, NULL);
+    g_hash_table_foreach(self->by_business_id, free_key, NULL);
+    g_hash_table_destroy(self->by_id);
+    g_hash_table_destroy(self->by_user_id);
+    g_hash_table_destroy(self->by_business_id);
+    free(self);
+  }
 }
