a39942eef68342a5a941fd2f1818008987ab9c70
Mariii-23 <marianadinis8@hotmail.com>
2021-06-19 22:24:21 +0100

    Add Comments to folder Controller -> Controller and IController

--- project_java/src/main/java/li3/grupo54/Controller/Controller.java
+++ project_java/src/main/java/li3/grupo54/Controller/Controller.java
@@ -11,11 +11,23 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Controller
+ */
 public class Controller implements IController {
-  private GestReviews model;
-  private IView view;
+  /**
+   * Model
+   */
+  private final GestReviews model;
+  /**
+   * View
+   */
+  private final IView view;
 
-  private List<IQueryController> queries;
+  /**
+   * Lista das Queries que poderam ser executadas
+   */
+  private final List<IQueryController> queries;
 
   public Controller(GestReviews model, IView view) {
     this.model = model;
@@ -26,12 +38,19 @@ public class Controller implements IController {
     this.view.setOnRestore(this::readObjectFile);
   }
 
+  /**
+   * Adicionar uma determinada Query
+   * @param controller Controller
+   */
   @Override
   public void addQuery(IQueryController controller) {
     view.addQuery(controller.getView(), controller::executeAndShow);
     queries.add(controller);
   }
 
+  /**
+   * Executar o controller
+   */
   @Override
   public void executa() {
     view.show();
@@ -41,10 +60,20 @@ public class Controller implements IController {
     //}
   }
 
+  /**
+   * Guarda o model como objeto num dado ficheiro.
+   * @param file Ficherio a escrever
+   * @throws IOException
+   */
   public void saveObjectFile(File file) throws IOException {
     this.model.onSave(file.getAbsolutePath());
   }
 
+  /**
+   * Método que efetua o load do model a partir do caminho para três ficheiros.
+   * User, Business e Reviews.
+   * @param triple FileTriple
+   */
   private void loadTriple(FileTriple triple) {
     try {
       model.load(triple.getUsersFile(), triple.getBusinessesFile(), triple.getReviewsFile());
@@ -54,6 +83,11 @@ public class Controller implements IController {
       view.showError("Error opening file", e.getMessage());
     }
   }
+
+  /**
+   * Lê um determinado ficheiro de objetos, colocando isso no model.
+   * @param file Ficheiro a ler
+   */
    private void readObjectFile(File file) {
      try {
        this.model.onRestore(file.getAbsolutePath());
--- project_java/src/main/java/li3/grupo54/Controller/IController.java
+++ project_java/src/main/java/li3/grupo54/Controller/IController.java
@@ -2,8 +2,19 @@ package li3.grupo54.Controller;
 
 import li3.grupo54.Controller.Queries.IQueryController;
 
+/**
+ * Interface do Controller
+ */
 public interface IController {
+
+  /**
+   * Método que permite adicionar uma query.
+   * @param controller Controller
+   */
   void addQuery(IQueryController controller);
 
+  /**
+   * Método que permite a execução do controller.
+   */
   void executa();
 }
